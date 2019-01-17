0cd3911e8d954727ede2edeb4155155faf6eb3cd
yorgosk <kgeoinvest@gmail.com>
2019-01-17 13:16:56 +0200

    added statistical analysis in output processing

--- openpose_ros_receiver/scripts/process_output.py
+++ openpose_ros_receiver/scripts/process_output.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import os
-import glob
 import re
 import math
 import time
-	
+import numpy as np
+from scipy import stats
+
 '''
 Get a list of keys from dictionary which has the given value
 '''
@@ -47,9 +48,10 @@ if __name__ == "__main__":
         fp = open(csv_folder_path + value + ".csv", 'w')
         fp.close()
     
-    # create our 3d report matrix: [BodyPart][x/y/z/prob][t1,...,t10,mean,variance] --> 25 * 4 * 12
-    part, elem, val = 25, 4, 12
-    report_matrix = [ [ [ float('nan') for k in range(val) ] for j in range(elem) ] for i in range(part) ]
+    # create our 3d report matrix: [BodyPart][x/y/z/prob][t0,...,t9,mean,variance,skewness,kurtosis] --> 25 * 4 * 17
+    part, elem, val = 25, 4, 17
+    stat_analysis_idx, nobs_idx, min_idx, max_idx, mean_idx, variance_idx, skewness_idx, kurtosis_idx = 10, 10, 11, 12, 13, 14, 15, 16
+    report_matrix = [ [ [ np.nan for k in range(val) ] for j in range(elem) ] for i in range(part) ]
 
     # access the files of the output directory
     file_counter = 0
@@ -84,9 +86,29 @@ if __name__ == "__main__":
         except Exception as e:
             raise e
 
+    # do statistical analysis
+    for i in range(part):
+        for j in range(elem):
+            # count the non-nan values
+            non_nans = (~np.isnan(report_matrix[i][j][0:stat_analysis_idx])).sum(0)
+            if not non_nans:
+                continue
+
+            # source: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.describe.html
+            description = stats.describe(report_matrix[i][j][0:stat_analysis_idx])
+            report_matrix[i][j][nobs_idx] = description.nobs
+            report_matrix[i][j][min_idx] = np.nanmin(report_matrix[i][j][0:stat_analysis_idx], axis=0)
+            report_matrix[i][j][max_idx] = np.nanmax(report_matrix[i][j][0:stat_analysis_idx], axis=0)
+            report_matrix[i][j][mean_idx] = description.mean
+            report_matrix[i][j][variance_idx] = description.variance
+            report_matrix[i][j][skewness_idx] = description.skewness
+            report_matrix[i][j][kurtosis_idx] = description.kurtosis
+
+    # write statistical analysis report
     for i in range(part):
        # write in the appropriate CSV
         with open(csv_folder_path + body_25_body_parts_dict.get(i) + ".csv", 'a') as fp:
+            print >> fp , "elem,t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,nobs,min,max,mean,variance,skewness,kurtosis"
             for j in range(elem):
                 print >> fp , element_dict.get(j) + "," + (",".join( str(e) for e in report_matrix[i][j] ))
 
