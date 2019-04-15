accde21770161bd3ced1ed76dd91b6c08fff396a
yorgosk <kgeoinvest@gmail.com>
2019-04-15 15:45:27 +0300

    more on documentation

--- openpose_ros_respond/include/openpose_ros_respond.hpp
+++ openpose_ros_respond/include/openpose_ros_respond.hpp
@@ -29,6 +29,8 @@ public:
     /* Class constructors */
     HumanLimbGesture();
     HumanLimbGesture(const geometry_msgs::Point bodyPositionInSpace, const std::vector<geometry_msgs::Point> limbPositionInSpace);
+    /* Print limb gesture description */
+    void print();
 };
 
 /* Node class */
@@ -38,6 +40,7 @@ private:
     ros::NodeHandle nh_;
     std::string robot_frame_coords_str_topic_, robot_frame_coords_msg_topic_;
     int queue_size_, human_body_keypoints_;
+    double deviation_margin_;
     ros::Subscriber subRobotFrameCoordsStr_, subRobotFrameCoordsMsg_;
 public:
     /* Node functions */
@@ -49,7 +52,7 @@ public:
     /* Human body keypoint coordinates in the robot's coordinate frams as regular messages */
     void robotFrameCoordsMsgTopicCallback(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg);
     /* Approximate a human body limb's gesture as a series of points */
-    HumanLimbGesture ApproximateLimbGesture(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg);
+    HumanLimbGesture ApproximateLimbGesture(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg, const std::vector<geometry_msgs::Point>& limb);
     /* Associate the human body's position in space with a single keypoint */
     geometry_msgs::Point BodyPositionInSpace(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg);
     /* Approximate a human body limb's position in space with a series of keypoints */
--- openpose_ros_respond/src/openpose_ros_respond.cpp
+++ openpose_ros_respond/src/openpose_ros_respond.cpp
@@ -15,6 +15,16 @@ HumanLimbGesture::HumanLimbGesture(const geometry_msgs::Point bodyPositionInSpac
     ;
 }
 
+/* Print limb gesture description */
+void HumanLimbGesture::print()
+{
+    ROS_INFO("Body position in space: (x, y, z) = (%f, %f, %f)", bodyPositionInSpace_.x, bodyPositionInSpace_.y, bodyPositionInSpace_.z);
+
+    ROS_INFO("Limb position in space:");
+    for (uint8_t i = 0; i < limbPositionInSpace_.size(); i++)
+        ROS_INFO("i = %d, (x, y, z) = (%f, %f, %f)", i, limbPositionInSpace_.at(i).x, limbPositionInSpace_.at(i).y, limbPositionInSpace_.at(i).z);
+}
+
 /* Node class */
 
 /* Class constructor -- ROS node initializer */
@@ -25,9 +35,10 @@ OpenPoseROSRespond::OpenPoseROSRespond()
     nh_.param("openpose_ros_respond_node/robot_frame_coords_msg_topic", robot_frame_coords_msg_topic_, std::string("/openpose_ros_receiver/robot_frame_coords_msg"));
     nh_.param("openpose_ros_respond_node/queue_size", queue_size_, 2);
     nh_.param("openpose_ros_respond_node/human_body_keypoints", human_body_keypoints_, 25);
+    nh_.param("openpose_ros_respond_node/deviation_margin", deviation_margin_, 0.15);
 
     /* Subscribe to robot frame coordinates topics */
-    subRobotFrameCoordsStr_ = nh_.subscribe(robot_frame_coords_str_topic_, queue_size_, &OpenPoseROSRespond::robotFrameCoordsStrTopicCallback, this);
+    // subRobotFrameCoordsStr_ = nh_.subscribe(robot_frame_coords_str_topic_, queue_size_, &OpenPoseROSRespond::robotFrameCoordsStrTopicCallback, this);
     subRobotFrameCoordsMsg_ = nh_.subscribe(robot_frame_coords_msg_topic_, queue_size_, &OpenPoseROSRespond::robotFrameCoordsMsgTopicCallback, this);
 
     ros::spin();
@@ -42,24 +53,45 @@ void OpenPoseROSRespond::robotFrameCoordsStrTopicCallback(const std_msgs::String
 /* Human body keypoint coordinates in the robot's coordinate frams as regular messages */
 void OpenPoseROSRespond::robotFrameCoordsMsgTopicCallback(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg)
 {
-    ;
+    /* for each limb */
+    for (auto const& limbMapping : POSE_BODY_25_LIMBS_NAME_TO_IDX)
+    {
+        /* limb keypoints pre-processing */
+        std::vector<geometry_msgs::Point> limb;
+
+        /* for debugging */
+        ROS_INFO("%s", limbMapping.first.c_str());
+        for (auto const& keypointIdx : limbMapping.second)
+        {
+            /* for debugging */
+            ROS_INFO("%d", keypointIdx);
+            /* gather limb keypoints */
+            geometry_msgs::Point tempPoint;
+            tempPoint.x = msg->body_key_points_with_prob[keypointIdx].x;
+            tempPoint.y = msg->body_key_points_with_prob[keypointIdx].y;
+            tempPoint.z = msg->body_key_points_with_prob[keypointIdx].z;
+            limb.push_back(tempPoint);
+        }
+
+        /* approximate limbs gesture as a series of points */
+        HumanLimbGesture humanLimbGesture;
+        humanLimbGesture = ApproximateLimbGesture(msg, limb);
+
+        /* for debugging */
+        humanLimbGesture.print();
+
+        /* TODO: respond to the limb gesture */
+    }
 }
 
 /* Approximate a human body limb's gesture as a series of points */
-HumanLimbGesture OpenPoseROSRespond::ApproximateLimbGesture(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg)
+HumanLimbGesture OpenPoseROSRespond::ApproximateLimbGesture(const openpose_ros_receiver_msgs::OpenPoseReceiverHuman::ConstPtr& msg, const std::vector<geometry_msgs::Point>& limb)
 {
     /* human body's position in space as a single point */
     geometry_msgs::Point bodyPositionInSpace;
-
     bodyPositionInSpace = BodyPositionInSpace(msg);
 
-    /* limb keypoints pre-processing */
-    std::vector<geometry_msgs::Point> limb;
-
-    /* TODO: gather limb keypoints */
-
     std::vector<geometry_msgs::Point> limbPositionInSpace;
-
     limbPositionInSpace = LimbPositionInSpace(limb);
 
     HumanLimbGesture humanLimbGesture(bodyPositionInSpace, limbPositionInSpace);
@@ -72,7 +104,7 @@ geometry_msgs::Point OpenPoseROSRespond::BodyPositionInSpace(const openpose_ros_
 {
     geometry_msgs::Point p; 
 
-    for (int i = 0; i < human_body_keypoints_; i++)
+    for (uint8_t i = 0; i < human_body_keypoints_; i++)
     {
         if (msg->body_key_points_with_prob[i].prob)  // at least one is enough to be a valid keypoint (since (0,0,0) is our robot's base link position)
         {
